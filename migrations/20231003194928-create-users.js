d9244b3bd521e3092b46409c6afb343a25ef7199
sepehr eslami <sepehr1381eslami@gmail.com>
2023-10-03 23:22:04 +0330

    add users db model

---
+++ migrations/20231003194928-create-users.js
@@ -0,0 +1,45 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      phone_number: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      first_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      birth_year: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
